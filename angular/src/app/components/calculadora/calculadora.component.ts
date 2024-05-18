b8ae85084c1a77b45a2e51bb21e51b8daedbe070
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-05-17 23:00:45 -0500

    Trabajado en clase

--- angular/src/app/components/calculadora/calculadora.component.ts
+++ angular/src/app/components/calculadora/calculadora.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-calculadora',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
+  
+  /* @Output() */ resultadoHijo: number = 0;
+  sumar(){
+    this.resultadoHijo = this.resultadoHijo + 1
+  }
+  
+  restar(){
+    this.resultadoHijo = this.resultadoHijo - 1
+  }
+
 
 }
--- angular/src/app/components/inicio/inicio.component.ts
+++ angular/src/app/components/inicio/inicio.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { CalculadoraComponent } from "../calculadora/calculadora.component"
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CalculadoraComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
-
+  
+  numero: number = 0;
+  sumar(){
+    this.numero = this.numero + 2
+  }
+  
+  restar(){
+    this.numero = this.numero - 2
+  }
+  
+  //resultadoX: Number = 10;
 }
